e810c78e4b3758e1ff873de19aa0c9669431d3aa
oceanesch <oceane.sch@gmail.com>
2022-03-10 14:46:47 -0500

    create react app

---
+++ front/src/App.js
@@ -0,0 +1,11 @@
+import MainNavigation from './components/MainNavigation/MainNavigation';
+
+function App() {
+  return (
+    <div>
+      <MainNavigation />
+    </div>
+  );
+}
+
+export default App;
