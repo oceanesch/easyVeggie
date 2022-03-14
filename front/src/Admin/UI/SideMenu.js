b52acd13cbc9f92551963cedd5aa2c7ef0bb5357
oceanesch <oceane.sch@gmail.com>
2022-03-14 14:40:39 -0400

    working on the css of the side menu

--- front/src/Admin/UI/SideMenu.js
+++ front/src/Admin/UI/SideMenu.js
@@ -1,15 +1,15 @@
-import { useState } from 'react';
-import SwipeableDrawer from '@mui/material/SwipeableDrawer';
+import React, { useState } from 'react';
+import { StyledEngineProvider } from '@mui/material/styles';
+import Drawer from '@mui/material/Drawer';
 import List from '@mui/material/List';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemText from '@mui/material/ListItemText';
 import ExpandLess from '@mui/icons-material/ExpandLess';
 import ExpandMore from '@mui/icons-material/ExpandMore';
 import Collapse from '@mui/material/Collapse';
-import { ListItem } from '@mui/material';
+import classes from './SideMenu.module.css';
 
 const SideMenu = () => {
-  const [openSideMenu, setOpenSideMenu] = useState(false);
   const [openNestedMenu, setOpenNestedMenu] = useState({
     openNutritionalNestedMenu: false,
     openRecipeNestedMenu: false,
@@ -44,10 +44,6 @@ const SideMenu = () => {
     });
   };
 
-  const toggleSideMenuHandler = () => {
-    setOpenSideMenu(!setOpenSideMenu);
-  };
-
   const menu = [
     {
       key: 1,
@@ -76,39 +72,47 @@ const SideMenu = () => {
   ];
 
   return (
-    <SwipeableDrawer
-      anchor="left"
-      open={openSideMenu}
-      onOpen={toggleSideMenuHandler}
-      onClose={toggleSideMenuHandler}
-      variant="permanent"
-    >
-      <List>
-        {menu.map((menuItem) => {
-          return (
-            <div key={menuItem.key}>
-              <ListItemButton onClick={menuItem.toggleFunction}>
-                <ListItemText primary={menuItem.primaryName} />
-                {menuItem.state ? <ExpandLess /> : <ExpandMore />}
-              </ListItemButton>
-              <Collapse in={menuItem.state} timeout="auto" unmountOnExit>
-                <List>
-                  <ListItemButton>
-                    <ListItemText primary={menuItem.nestedList1} />
-                  </ListItemButton>
-                  <ListItemButton>
-                    <ListItemText primary={menuItem.nestedList2} />
-                  </ListItemButton>
-                </List>
-              </Collapse>
-            </div>
-          );
-        })}
-        <ListItemButton>
-          <ListItemText primary="MANAGE USER" />
-        </ListItemButton>
-      </List>
-    </SwipeableDrawer>
+    <StyledEngineProvider injectFirst>
+      <Drawer anchor="left" variant="permanent">
+        <List>
+          {menu.map((menuItem) => {
+            return (
+              <div key={menuItem.key} className={classes.sideMenuButton}>
+                <ListItemButton onClick={menuItem.toggleFunction}>
+                  <ListItemText
+                    primary={menuItem.primaryName}
+                    className={classes.sideMenuText}
+                  />
+                  {menuItem.state ? <ExpandLess /> : <ExpandMore />}
+                </ListItemButton>
+                <Collapse in={menuItem.state} timeout="auto" unmountOnExit>
+                  <List>
+                    <ListItemButton>
+                      <ListItemText
+                        primary={menuItem.nestedList1}
+                        className={`${classes.sideMenuText} ${classes.nestedListItem}`}
+                      />
+                    </ListItemButton>
+                    <ListItemButton>
+                      <ListItemText
+                        primary={menuItem.nestedList2}
+                        className={`${classes.sideMenuText} ${classes.nestedListItem}`}
+                      />
+                    </ListItemButton>
+                  </List>
+                </Collapse>
+              </div>
+            );
+          })}
+          <ListItemButton>
+            <ListItemText
+              primary="MANAGE USER"
+              className={classes.sideMenuText}
+            />
+          </ListItemButton>
+        </List>
+      </Drawer>
+    </StyledEngineProvider>
   );
 };
 
