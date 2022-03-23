fc23334122b3fe9990384ede19f880a6a5868c59
oceanesch <oceane.sch@gmail.com>
2022-03-23 10:20:55 -0400

    refactoring code

--- front/src/Nutritional Record Page/Page/NutritionalRecordsPage.js
+++ front/src/Nutritional Record Page/Page/NutritionalRecordsPage.js
@@ -1,15 +1,7 @@
-import {
-  Typography,
-  Grid,
-  FormControl,
-  InputLabel,
-  Select,
-  MenuItem,
-} from '@mui/material';
+import { Typography, Grid } from '@mui/material';
 import { StyledEngineProvider } from '@mui/material/styles';
 import sharedClasses from '../../shared/sharedCss.module.css';
-import classes from './NutritionalRecordsPage.module.css';
-import { SearchBar } from '../../shared';
+import SearchSection from '../Features/SearchSection';
 
 const NutritionalRecordsPage = () => {
   return (
@@ -18,68 +10,7 @@ const NutritionalRecordsPage = () => {
         <Typography component="h1" className={sharedClasses.mainTitle}>
           NUTRITIONAL RECORDS
         </Typography>
-        <Grid item className={sharedClasses.searchSection}>
-          <SearchBar />
-        </Grid>
-        <Grid container className={classes.filterSection}>
-          <Grid item>
-            <Typography component="h2" className={sharedClasses.subTitle}>
-              FILTER BY CATEGORY OF FOOD
-            </Typography>
-            <FormControl fullWidth className={classes.filterSelect}>
-              <InputLabel id="select-category-food-label">
-                Select a category of food
-              </InputLabel>
-              <Select
-                labelId="select-category-food-label"
-                id="select-category-food"
-                label="Select a category of food"
-                onChange={(event) => {
-                  console.log(event.target.value);
-                }}
-              >
-                <MenuItem>Beans & Legumes</MenuItem>
-                <MenuItem>Fruits</MenuItem>
-                <MenuItem>Grains</MenuItem>
-                <MenuItem>Nuts</MenuItem>
-                <MenuItem>Seeds</MenuItem>
-                <MenuItem>Soy Product</MenuItem>
-                <MenuItem>Spices</MenuItem>
-                <MenuItem>Vegetables</MenuItem>
-              </Select>
-            </FormControl>
-          </Grid>
-          <Grid item>
-            <Typography component="h2" className={sharedClasses.subTitle}>
-              FILTER BY NUTRIENT
-            </Typography>
-            <FormControl fullWidth className={classes.filterSelect}>
-              <InputLabel id="select-nutrient-label">
-                Select a nutrient
-              </InputLabel>
-              <Select
-                labelId="select-nutrient-label"
-                id="select-nutrient"
-                label="Select a nutrient"
-                onChange={(event) => {
-                  console.log(event.target.value);
-                }}
-              >
-                <MenuItem>Calcium</MenuItem>
-                <MenuItem>Cholesterol</MenuItem>
-                <MenuItem>Fat</MenuItem>
-                <MenuItem>Fiber</MenuItem>
-                <MenuItem>Iron</MenuItem>
-                <MenuItem>Potassium</MenuItem>
-                <MenuItem>Protein</MenuItem>
-                <MenuItem>Sodium</MenuItem>
-                <MenuItem>Sugar</MenuItem>
-                <MenuItem>Vitamin</MenuItem>
-                <MenuItem>Zinc</MenuItem>
-              </Select>
-            </FormControl>
-          </Grid>
-        </Grid>
+        <SearchSection />
       </Grid>
     </StyledEngineProvider>
   );
---
+++ front/src/Nutritional Record Page/UI/FoodSelect.js
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+import { StyledEngineProvider } from '@mui/material/styles';
+import { FormControl, InputLabel, Select, MenuItem } from '@mui/material';
+import sharedClasses from '../../shared/sharedCss.module.css';
+
+const FoodSelect = () => {
+  const [selectedFood, setSelectedFood] = useState('');
+
+  return (
+    <StyledEngineProvider injectFirst>
+      <FormControl fullWidth className={sharedClasses.filterSelect}>
+        <InputLabel id="select-category-food-label">
+          Select a category of food
+        </InputLabel>
+        <Select
+          labelId="select-category-food-label"
+          id="select-category-food"
+          label="Select a category of food"
+          value={selectedFood}
+          onChange={(event) => {
+            console.log(event.target.value);
+            setSelectedFood(event.target.value);
+          }}
+        >
+          <MenuItem value={'Beans & Legumes'}>Beans & Legumes</MenuItem>
+          <MenuItem value={'Fruits'}>Fruits</MenuItem>
+          <MenuItem value={'Grains'}>Grains</MenuItem>
+          <MenuItem value={'Nuts'}>Nuts</MenuItem>
+          <MenuItem value={'Seeds'}>Seeds</MenuItem>
+          <MenuItem value={'Soy Products'}>Soy Products</MenuItem>
+          <MenuItem value={'Spices'}>Spices</MenuItem>
+          <MenuItem value={'Vegetables'}>Vegetables</MenuItem>
+        </Select>
+      </FormControl>
+      ;
+    </StyledEngineProvider>
+  );
+};
+
+export default FoodSelect;
