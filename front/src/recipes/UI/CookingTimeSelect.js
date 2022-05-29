477d3c69ac2235d1ee8ea96034507a034a98a370
oceanesch <oceane.sch@gmail.com>
2022-05-29 13:59:03 +0200

    working on the filter selects

--- front/src/recipes/UI/CookingTimeSelect.js
+++ front/src/recipes/UI/CookingTimeSelect.js
@@ -1,3 +1,44 @@
-const CookingTimeSelect = () => {};
+import {
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  StyledEngineProvider,
+} from '@mui/material';
+import { useState } from 'react';
+import sharedClasses from '../../shared/sharedCss.module.css';
+
+const CookingTimeSelect = () => {
+  const [selectedCookingTime, setSelectedCookingTime] = useState('');
+
+  return (
+    <StyledEngineProvider injectFirst>
+      <FormControl
+        fullWidth
+        className={sharedClasses.filterSelect}
+        size="small"
+      >
+        <InputLabel id="select-cooking-time-label">COOKING TIME</InputLabel>
+        <Select
+          labelId="select-cooking-time-label"
+          id="select-cooking-time"
+          label="Select a cooking time"
+          value={selectedCookingTime}
+          onChange={(event) => {
+            console.log(event.target.value);
+            setSelectedCookingTime(event.target.value);
+          }}
+        >
+          <MenuItem value={'10 min or less'}>{`10 min or less`}</MenuItem>
+          <MenuItem value={'15 min or less'}>{`15 min or less`}</MenuItem>
+          <MenuItem value={'20 min or less'}>{`20 min or less`}</MenuItem>
+          <MenuItem value={'30 min or less'}>{`30 min or less`}</MenuItem>
+          <MenuItem value={'40 min or less'}>{`40 min or less`}</MenuItem>
+          <MenuItem value={'50 min or less'}>{`50 min or less`}</MenuItem>
+        </Select>
+      </FormControl>
+    </StyledEngineProvider>
+  );
+};
 
 export default CookingTimeSelect;
--- front/src/recipes/features/SearchSection.js
+++ front/src/recipes/features/SearchSection.js
@@ -1,10 +1,34 @@
-import { StyledEngineProvider } from '@mui/material';
+import { Grid, StyledEngineProvider, Typography } from '@mui/material';
 import { SearchBar } from '../../shared';
+import AllergenSelect from '../UI/AllergenSelect';
+import CookingTimeSelect from '../UI/CookingTimeSelect';
+import RecipeTypeSelect from '../UI/RecipeTypeSelect';
+import sharedClasses from '../../shared/sharedCss.module.css';
 
 const SearchSection = () => {
   return (
     <StyledEngineProvider injectFirst>
       <SearchBar />
+      <Grid container className={sharedClasses.filterSection}>
+        <Grid item>
+          <Typography component="h2" className={sharedClasses.subTitle}>
+            FILTER BY TYPE OF RECIPE
+          </Typography>
+          <RecipeTypeSelect />
+        </Grid>
+        <Grid item>
+          <Typography component="h2" className={sharedClasses.subTitle}>
+            FILTER BY ALLERGEN
+          </Typography>
+          <AllergenSelect />
+        </Grid>
+        <Grid item>
+          <Typography component="h2" className={sharedClasses.subTitle}>
+            FILTER BY TYPE COOKING TIME
+          </Typography>
+          <CookingTimeSelect />
+        </Grid>
+      </Grid>
     </StyledEngineProvider>
   );
 };
