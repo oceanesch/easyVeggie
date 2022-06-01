19b41021eaef98124b0bc4b583b77ea28c8cd93c
oceanesch <oceane.sch@gmail.com>
2022-06-01 16:21:13 +0200

    finishing the recipe detail page

--- front/src/recipes/page/RecipeDetailPage.js
+++ front/src/recipes/page/RecipeDetailPage.js
@@ -1,12 +1,23 @@
-import { StyledEngineProvider } from '@mui/material';
+import { Grid, StyledEngineProvider } from '@mui/material';
+import { Footer } from '../../shared';
+import RecipeDetailCookingDirection from '../features/RecipeDetailCookingDirection';
 import RecipeDetailHeadSection from '../features/RecipeDetailHeadSection';
 import RecipeDetailIngredientSection from '../features/RecipeDetailIngredientSection';
+import classes from './RecipeDetailPage.module.css';
 
 const RecipeDetailPage = () => {
   return (
     <StyledEngineProvider injectFirst>
       <RecipeDetailHeadSection />
-      <RecipeDetailIngredientSection />
+      <Grid container className={classes.recipeDetailMainSection}>
+        <Grid item>
+          <RecipeDetailIngredientSection />
+        </Grid>
+        <Grid item>
+          <RecipeDetailCookingDirection />
+        </Grid>
+      </Grid>
+      <Footer />
     </StyledEngineProvider>
   );
 };
